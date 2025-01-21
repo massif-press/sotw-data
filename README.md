989da991a0be13fe3a70540402212ba78a25e2a4
John A <john.arena.3@gmail.com>
2025-01-20 19:36:38 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Player-facing Shadow of the Wolf Data for Comp/Con
+
+## Installation
+
+```js
+npm add @massif/sotw-data
+//or
+yarn add @massif/sotw-data
+```
+
+## Usage
+
+```js
+import * as sotw from '@massif/sotw-data';
+
+const sotwFrameData = sotw.frames;
+```
+
+## Building LCP
+
+```js
+yarn && yarn build
+// or
+npm install && npm run build
+```
+
+produces a versioned LCP file in `dist/`
---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const data = {
+  lcp_manifest: require('./lib/lcp_manifest.json'),
+  backgrounds: require('./lib/backgrounds.json'),
+  bonds: require('./lib/bonds.json'),
+  frames: require('./lib/frames.json'),
+  npc_features: require('./lib/npc_features.json'),
+  pilot_gear: require('./lib/pilot_gear.json'),
+  systems: require('./lib/systems.json'),
+};
+
+module.exports = data;
